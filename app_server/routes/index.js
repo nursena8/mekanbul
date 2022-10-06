13433a0caac83e1142dc8e7afc53a43e2c4508cb
nursena baykir <nursenabaykir@gmail.com>
2022-10-06 15:06:47 +0300

    ilk gonderim

---
+++ app_server/routes/index.js
@@ -0,0 +1,8 @@
+var express = require('express');
+var router = express.Router();
+var ctrMain=require('../controllers/main');
+
+/* GET home page. */
+router.get('/',ctrMain.index);
+
+module.exports = router;
