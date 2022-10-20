ce25cb7e721c87a0f5ad0c7cde34e2e6b0e80c2e
sena070 <clear>
2022-10-20 15:49:44 +0300

    ödev3

--- app_server/controllers/mekanlar.js
+++ app_server/controllers/mekanlar.js
@@ -2,11 +2,80 @@ var express = require('express');
 var router = express.Router();
 
 const anaSayfa=function(req, res, next) {
-    res.render('anasayfa', { title: 'Ana sayfa' });
+    res.render('anasayfa', {
+       "baslik": "Anasayfa",
+       "sayfaBaslik":{
+         "siteAd":"MeakanBul",
+         "slogan":"Civardaki mekanları keşfet"
+       },
+       "mekanlar":[
+         {
+           "ad":"Sturbucks",
+           "adres":"SDÜ Batı Kampüsü",
+           "puan":"4",
+           "imkanlar":["Dünya kahvesi","kekler","pastalar"],
+           "mesafe":"2km"
+         }
+         ,
+         {
+          "ad":"Gloria Jeans",
+          "adres":"iyaş",
+          "puan":"3",
+          "imkanlar":["kahve","cay","pasta"],
+          "mesafe":"10km"
+         }
+       ] 
+     }
+     );
   }
 
   const mekanBilgisi=function(req, res, next) {
-    res.render('mekanbilgisi', { title: 'Mekan bilgisi' });
+    res.render('mekanbilgisi',
+     { "baslik": "Mekan bilgisi",
+      "mekanBaslik":"Starbucks",
+      "mekanDetay":{
+        "ad":"Starbucks",
+        "adres":"centrum garden",
+        "puan":"4",
+        "imkanlar":["çaylar","pastalar"],
+        "koordinatlar":{
+          "enlem":"37.7",
+          "boylam":"30.5"
+          },
+
+        "saatler":[
+          {
+            "gunler":"pazartesi-cuma",
+            "acilis":"9:00",
+            "kapanis":"22:00",
+            "kapali":false
+          },
+          {
+            "gunler":"cumartesi-pazar",
+            "acilis":"9:00",
+            "kapanis":"22:00",
+            "kapali":false
+          }
+        ],
+        
+          "yorumlar":[
+            {
+              "yorumYapan":"Sena",
+              "puan":"3",
+              "tarih":"20 eki 2022",
+              "yorumMetini":"Süper"
+            },
+            {
+              "yorumYapan":"isimsiz",
+              "puan":"5",
+              "tarih":"20 eki 2022",
+              "yorumMetini":"Süper"
+            }
+          ]
+        
+
+      }
+     });
   }
 
   const yorumEkle=function(req, res, next) {
