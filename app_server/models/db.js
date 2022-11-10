e7be70621a0e1a48e3ee0a6cfd8684ced1d78860
sena070 <clear>
2022-11-10 16:00:56 +0300

    odev

---
+++ app_server/models/db.js
@@ -0,0 +1,27 @@
+var mongoose=require("mongoose");
+var dbURI="mongodb://localhost/mekanbul";
+
+mongoose.connect(dbURI);
+function kapat(msg,callback){
+    mongoose.connection.close(function(){
+        console.log(msg);
+    callback();
+    });
+    
+}
+process.on("SIGINT",function(){
+    kapat("uygulama kapatıldı",function(){
+        process.exit(0);
+    });
+});
+mongoose.connection.on("connected",function(){
+    console.log(dbURI+"adresindeki veritabanına bağlandı.");
+});
+mongoose.connection.on("disconnected",function(){
+    console.log("balantı koptu");
+});
+mongoose.connection.on("errorr",function(){
+    console.log("balantı hatası");
+});
+
+require("./mekansema");
