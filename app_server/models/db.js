b14af641acc62a27d5eadc1128ced7f8c7ae3017
sena070 <clear>
2022-11-10 16:12:58 +0300

    odev

--- app_server/models/db.js
+++ app_server/models/db.js
@@ -1,5 +1,6 @@
 var mongoose=require("mongoose");
-var dbURI="mongodb://localhost/mekanbul";
+//var dbURI="mongodb://localhost/mekanbul";
+var dbURI="mongodb+srv://sena:1456@mekanbul.hwalpwa.mongodb.net/?retryWrites=true&w=majority"
 
 mongoose.connect(dbURI);
 function kapat(msg,callback){
